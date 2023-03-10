c569875455d19b37b9fbb7067c36f763f9701ea7
Fabien <fabien.sens@free.fr>
2023-03-10 18:58:07 +0100

    Implementing Footer Gallery FindUs

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { FooterOverlay, Newsletter } from '../../components';
+import { FiFaceBook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
 import './Footer.css';
 
 const Footer = () => (
-  <div>
-    Footer
-  </div>
+	<div className="app__footer section__padding">
+		<FooterOverlay />
+		<Newsletter />
+	</div>
 );
 
 export default Footer;
